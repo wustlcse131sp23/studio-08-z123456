3deab91376e6a91760b852d52d1b3de60649cea7
10370 <10370@TABLET-O13EIHM1.attlocal.net>
2023-04-11 01:35:44 -0500

    123

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,54 @@
 package studio8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Date {
+	private int month;
+	private int year;
+	private int day;
+	private String[] lst;
+	public Date(int month1,int year1, int day1){
+		month=month1;
+		year=year1;
+		day=day1;
+		lst=new String[2];//**
+		lst[0]="2019-01-01";
+		lst[1]="2019-01-02";
+	}
+	public String to_String() {
+		String Smonth;
+		String Sday;
+		String Syear=""+year;
 
-
+		if (month >=10) {
+			Smonth=""+month;
+		}
+		else {
+			Smonth="0"+month;
+		}
+		if (day>=10) {
+			Sday=""+day;
+		}
+		else {
+			Sday="0"+day;
+		}
+		return Syear+"-"+Smonth+"-"+Sday;
+	}
+	
+	public boolean is_holiday() {
+		String Sdate=to_String();
+		for (int i=0;i<=lst.length-1;i++) {
+			if (Sdate.equals(lst[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
     public static void main(String[] args) {
+    	Date class1=new Date(1,2019,1);
 
+    	System.out.println(class1.is_holiday());
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,7 +2,12 @@ package studio8;
 
 
 public class Time {
-
+	private int hours;
+	private int minutes;
+	private boolean format_24;
+	public Time(int hours, int minutes, boolean format_24) {
+		
+	}
 	public static void main(String[] args) {
  
     	
